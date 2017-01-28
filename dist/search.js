b0d16529e1acd6cd7aacf9e14da67c85f6cceb57
Raymond <ray@firestitch.com>
2017-01-28 12:30:13 -0500

    added fsSearch

--- dist/search.js
+++ dist/search.js
@@ -36,4 +36,42 @@
         };
     });
 })();
+(function () {
 
+    angular.module('fs-angular-search')
+	.provider("fsSearch",function() {
+
+		var _options = {}, _instances = {};
+		this.options = function(options) {
+			_options = options;
+		}
+
+		this.$get = function($rootScope) {
+
+			var service = {
+				options: options,
+				reload: reload,
+				add: add,
+				get: get
+			 };
+
+			return service;
+
+			function options() {
+				return _options;
+			}
+
+			function reload(name) {
+				$rootScope.$broadcast('search-' + name,{ action: 'reload' });
+			}
+
+			function add(id, instance) {
+				return _instances[id] = instance;
+			}
+
+			function get(id) {
+				return _instances[id];
+			}
+		}
+	});
+})();
